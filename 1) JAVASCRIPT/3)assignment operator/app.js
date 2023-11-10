5966502d77a1f4284da106ea9d3c6d00948af404
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2023-11-10 23:08:56 +0500

    assignment operators

---
+++ 1) JAVASCRIPT/3)assignment operator/app.js
@@ -0,0 +1,12 @@
+var a = 10;
+var b = 3;
+// a+=b;
+// document.write(a);
+// a-=b;
+// document.write(a);
+// a/=b;
+// document.write(a);
+// a*=b;
+// document.write(a);
+a %= b;
+document.write(a);
