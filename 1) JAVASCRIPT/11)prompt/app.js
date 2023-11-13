4bbdeb63d9df9be03e3543beb875a623da432d01
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2023-11-13 14:31:24 +0500

    11, 12 & 13

---
+++ 1) JAVASCRIPT/11)prompt/app.js
@@ -0,0 +1,7 @@
+var day=prompt("enter the day: ");
+
+if (day==1) {
+  document.write("friday");
+} else {
+  document.write("not a day");
+}
---
+++ 1) JAVASCRIPT/13)function parameters/app.js
@@ -0,0 +1,10 @@
+// function sum(a,b){
+//   document.write(a+b);
+// }
+// sum(10,20);
+
+//for default
+function sum(a = 9, b = 8) {
+  document.write(a + b);
+}
+sum();
