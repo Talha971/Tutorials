796e12d55d8bf9accb92ebef3d065a610621718b
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2023-11-14 16:12:03 +0500

    functions with return value

---
+++ 1) JAVASCRIPT/2-14) functions with return values/app.js
@@ -0,0 +1,29 @@
+
+    //return values are for use on another place
+
+        // function fullName(fn,ln){
+        //   var a = fn + " " + ln;
+        //
+        //   return a;
+        // }
+        // var fn = fullName("Talha","Tahir");
+        // document.write(fn);
+        // var fn2 = fullName("Talha","Tariq");
+        // document.write(fn2);
+
+      // another example
+
+      function sum(mths,sc,eng){
+        var a=mths+sc+eng;
+        return a;
+
+      }
+
+
+      function percentage(tt){
+        var per = tt/300 * 100;
+        document.write(per)
+
+      }
+      var total=sum(90,78,87);
+      percentage(total);
