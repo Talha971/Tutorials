578991af6cd9cc75d843bb17dcc9122605e069be
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2023-11-17 23:04:43 +0500

    nested loop

---
+++ 1) JAVASCRIPT/3-22) nested loop 2/app.js
@@ -0,0 +1,20 @@
+      // for (var i = 1; i <= 10; i++) {
+      // for(var j=1;j<=i;j++){
+      //   document.write(j+ " ");
+      // }
+      //   document.write("<br>")
+      // }
+
+      // for(var i = 1; i<=10; i++){
+      //   for(var j=1; j<=i; j++){
+      //   document.write(i + " ");
+      // }
+      // document.write("<br>");
+      // }
+
+      for(var i = 5; i>=1; i--){
+        for(var j=1; j<=i; j++){
+        document.write(i + " ");
+      }
+      document.write("<br>");
+      }
