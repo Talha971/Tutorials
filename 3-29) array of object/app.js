9b6b014e6c59f0b8e0a10308a8a04870e576cc5e
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2023-11-20 00:23:15 +0500

    array of objects

---
+++ 3-29) array of object/app.js
@@ -0,0 +1,12 @@
+var a = [
+  { name: "yyug", age: 34 },
+  { name: "yyunjxnskng", age: 34 },
+  { name: "yyugkjfhkjshd", age: 342 },
+];
+
+// document.write(a);
+console.log(a);
+
+for (var i = 0; i < a.length; i++) {
+  document.write(a[i].name + "     " + a[i].age + "<br>");
+}
