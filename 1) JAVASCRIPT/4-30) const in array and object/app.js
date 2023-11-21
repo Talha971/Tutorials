c0234c8775d6db2c975a902080861eee99f1dbe0
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2023-11-21 23:44:05 +0500

    const in array and objects

---
+++ 1) JAVASCRIPT/4-30) const in array and object/app.js
@@ -0,0 +1,10 @@
+const a = [1234, 5, 6, 7, 98];
+// we will not modify a because a is constant
+// a=[2,3,4,5,6,7];
+
+// same thing will happen in objects
+// you can modify values in array just for eg:
+
+a[1] = 21;
+console.log(a);
+// same thing will happen in objects
