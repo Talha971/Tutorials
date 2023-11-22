b27fca0d2b69815d6716d344f98c1a74292f982e
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2023-11-22 23:15:46 +0500

    string methods

---
+++ 1) JAVASCRIPT/4-33) strings methods/app.js
@@ -0,0 +1,121 @@
+var str = "lorem ipsum";
+
+// // length
+
+// var str = "lorem ipsum";
+// var a=str.length;
+// document.write(a);
+
+// // lower and upper case
+
+// var str = "loRem iPsum";
+// var a=str.toLowerCase();
+// document.write(a);
+// var a=str.toUpperCase();
+// document.write(a);
+
+// include
+// iska kaam hota words dhundna string main se
+
+var str = "loRem iPsume";
+// var a=str.includes("Rem");
+// document.write(a);
+
+// var a = str.includes("ipsum");
+// document.write(a);
+
+// var a=str.includes("loRem");
+// document.write(a);
+// -------------------
+
+// //start with and ends with
+
+// var a=str.startsWith("lorem");
+// document.write(a);
+
+// var a=str.endsWith("ipsumm");
+// document.write(a);
+
+// // // search
+// // // ye hamain matloob value ki position bata deta hai
+
+// var a=str.search("i");
+// document.write(a);
+
+// -------------------
+
+// // match
+// // ye wala function aapko matloob word ki array bana deta hai
+// //  g ka matlab hai globally
+// var a=str.match(/m/g);
+// document.write(a);
+
+// // index of last index of
+
+// var a=str.indexOf("e");
+// document.write(a);
+
+// var a=str.lastIndexOf("e");
+// document.write(a);
+
+// // Replace
+
+// var a=str.replace("lorem","chaman");
+// document.write(a+"<br>");
+// var a=str.replace(/m/g,"c");
+// document.write(a);
+
+// // trim
+// //it actually remove spaces
+// var b="        daskjkjaskl        ";
+// var c=b.trim();
+// alert(c);
+
+// // char at
+// // it shows the value at given index
+// var a=str.charAt(2);
+// document.write(a);
+
+// // charCodeAt()
+// // its function is to get the ascii value
+// var a=str.charCodeAt(1);
+// document.write(a);
+
+// //from char code()
+// // is ka kaam hota hai ascii code se  word nikkalna
+// var a=String.fromCharCode(100);
+// document.write(a);
+
+// // concat()
+// var str2="kesa hai";
+// var a=str.concat(str2);
+// document.write(a);
+
+// // split()
+
+// var a=str.split("o");
+// document.write(a);
+
+// // repeat
+// var a=str.repeat(3);
+// document.write(a);
+
+// slice()
+// var a=str.slice(1,5);
+// var a=str.slice("<br>"+ 0);
+// var a=str.slice(-2);
+// document.write(a);
+
+// //sub str
+// var a=str.substr(2,5);
+// document.write(a);
+
+// // // //sub str
+// var a=str.substring(2);
+// document.write(a);
+
+// // to string()
+// har alag datatype wali value ko string main convert kardeta HTMLDetailsElement
+
+// //valueof()
+// as it is value print kardeta HTMLDetailsElement
