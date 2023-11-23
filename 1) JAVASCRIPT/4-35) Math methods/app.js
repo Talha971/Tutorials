5580dd6b58dbb2e4b6d390715b9b4f9ad7663896
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2023-11-23 16:15:08 +0500

    Math method

---
+++ 1) JAVASCRIPT/4-35) Math methods/app.js
@@ -0,0 +1,69 @@
+// //  ceil() round off karta hai bari vali value uske baad
+        // // or agar hm - main karenge tou wou lower value dega
+        // var a=Math.ceil(2.3)
+        // document.write(a);
+
+
+        // // floor() ye hamesha choti value round off karke deta hai
+        // // or - laga ke higher value dega
+        // var a=Math.floor(2.9)
+        // document.write(a);
+
+
+        // // round() ye sahi wala round off karta hai
+        // var a=Math.round(2.5)
+        // document.write(a);
+
+        // // trunc() ye khali deciamal se pehle wali value deta hai
+        // var a=Math.trunc(3.7)
+        // document.write(a);
+
+
+        // // max()
+        // var a=Math.max(2.3,10,4,35,87)
+        // document.write(a);
+
+        // min()
+
+
+        // // sqrt() square root
+        // var a=Math.sqrt(49)
+        // document.write(a);
+
+
+        // // cbrt()
+        // var a=Math.cbrt(8)
+        // document.write(a);
+
+
+        // // pow(no , uski power)
+        // var a=Math.pow(2,4)
+        // document.write(a);
+
+
+        // // random()
+        // var a=Math.floor(Math.random()*10)+1;
+        // document.write(a);
+
+
+        // // abs() absolute value
+        // var a=Math.abs(2.89)
+        // document.write(a);
+
+        // //PI() 
+        // var a=Math.PI;
+        // document.write(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
