c05b1d8bb55f3b7921ba13431cd86d722cf92136
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2023-11-23 16:15:40 +0500

    Date methods

---
+++ 1) JAVASCRIPT/4-36) Date methods/app.js
@@ -0,0 +1,32 @@
+// //now  dates
+// var now = new Date();
+// document.write(now);
+
+// // to date string()
+// var now = new Date();
+// document.write(now.toDateString());
+
+// // get {date() month() year() day( )}
+var now = new Date();
+// // or
+// var now = new Date('16 january 2002');
+// document.write(now.getDate());
+// document.write(now.getMonth());
+// document.write(now.getFullYear());
+document.write(now.getDay());
+
+// // get hours() minutes() seconds() milliseconds()
+
+// var now = new Date();
+// document.write(now.getHours());
+// document.write(now.getMinutes());
+// document.write(now.getSeconds());
+// document.write(now.getMilliseconds());
+
+// set dates() months() etc
+
+// var now = new Date();
+// now.setFullYear(2021);
+// now.setHours(22);
+// now.setDate(2);
+// document.write(now.getDate() + "/" +now.getMonth()+"/"+now.getFullYear())
