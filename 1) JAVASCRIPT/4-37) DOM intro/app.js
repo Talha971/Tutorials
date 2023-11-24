3c7e4fea044f427f80016384614c69196532927c
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2023-11-24 21:41:00 +0500

    dom

---
+++ 1) JAVASCRIPT/4-37) DOM intro/app.js
@@ -0,0 +1,7 @@
+// var a=document.getElementById("sidebar")
+// console.log(a);
+
+// console.log(document.getElementsByClassName("list"))
+
+// array main daalne ka tarika
+console.log(document.getElementsByTagName("a")[1]);
