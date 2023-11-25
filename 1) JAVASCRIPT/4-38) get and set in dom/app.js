61c6d86876b8ab4597e746f6cab8eceb25b49686
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2023-11-25 23:46:07 +0500

    get and set in dom

---
+++ 1) JAVASCRIPT/4-38) get and set in dom/app.js
@@ -0,0 +1,103 @@
+////////////////////////////////////////////////////////////////////
+////////////////////////////GET/////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+
+// // inner text
+
+// var a = document.getElementById("footer").innerText;
+// console.log(a);
+
+// var a = document.getElementById("content").innerText;
+// console.log(a);
+
+////////////////////////////////////////////////////////////////////////////
+
+// // innerHTML
+
+// var a = document.getElementById("sidebar").innerHTML;
+// console.log(a);
+
+////////////////////////////////////////////////////////////////////////////
+
+// // get Attribute
+// var a = document.getElementById("sidebar").getAttribute("class");
+// console.log(a);
+
+// var a = document.getElementById("sidebar").getAttribute("style");
+// console.log(a);
+
+// var a = document.getElementById("sidebar").getAttribute("id");
+// console.log(a);
+
+////////////////////////////////////////////////////////////////////////////
+
+// // get attribute node
+
+// var a = document.getElementById("sidebar").getAttributeNode("class");
+// console.log(a);
+// var a = document.getElementById("sidebar").getAttributeNode("style");
+// console.log(a);
+// var a = document.getElementById("sidebar").getAttributeNode("id");
+// console.log(a);
+
+////////////////////////////////////////////////////////////////////////////
+
+// // attributes
+
+// var a = document.getElementById("sidebar").attributes;
+// console.log(a);
+
+// //[]
+// var a = document.getElementById("sidebar").attributes[1];
+// console.log(a);
+
+// //name
+// var a = document.getElementById("sidebar").attributes[1].name;
+// console.log(a);
+// // value
+// var a = document.getElementById("sidebar").attributes[1].value;
+// console.log(a);
+
+/////////////////////////////S//////////////////////////////////////
+/////////////////////////////E/////////////////////////////////////
+/////////////////////////////T///////////////////////////////////////
+
+// // innertext
+// var a = document.getElementById("header").innerText="hahahahhaha";
+// console.log(a);
+
+////////////////////////////////////////////////////////////////////////////
+
+// // innerHTML
+// var a = document.getElementById("header").innerHTML="<h1>hahahahhaha</h1>";
+// console.log(a);
+
+////////////////////////////////////////////////////////////////////////////
+
+// // setAttribute
+
+// class
+// document.getElementById("header").setAttribute("class", "abc");
+// var a = document.getElementById("header").getAttribute("class");
+// console.log(a);
+
+// style
+// document.getElementById("sidebar").setAttribute("style", " border: solid yellow 8px");
+// var a = document.getElementById("sidebar").getAttribute("style");
+// console.log(a);
+
+////////////////////////////////////////////////////////////////////////////
+
+// // attribute
+
+// var a=document
+//   .getElementById("sidebar")
+//   .attributes[0].value="bmc";
+// console.log(a);
+
+// // removeAttribute
+// document.getElementById("sidebar").removeAttribute("style");
+// var a = document.getElementById("sidebar").getAttribute("style");
+// console.log(a);
+
+////////////////////////////////////////////////////////////////////////////
