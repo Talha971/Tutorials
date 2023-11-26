1cb41ddbca11ce74b384b072369530c5a0a15da4
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2023-11-26 22:29:33 +0500

    target dom objects

---
+++ 1) JAVASCRIPT/4-39) target DOM object/app.js
@@ -0,0 +1,27 @@
+//    // class
+
+//       // querySelector
+// var a= document.querySelector(".list").innerHTML;
+// console.log(a)
+
+///////////////////////////////////////////////////////////////////////////////
+// // queryselector all
+// var a = document.querySelectorAll(".list")[0].innerHTML;
+// console.log(a);
+
+// // proving that querry selector chooses first element while queryselec all can select various value by given indexes
+
+//      // tag
+// var a = document.querySelector("li").innerHTML;
+// console.log(a);
+
+// var a = document.querySelectorAll("li");
+// console.log(a);
+
+// // advanced letter
+
+// var a = document.querySelector("#sidebar li");
+// console.log(a);
+
+// var a = document.querySelectorAll("#sidebar li");
+// console.log(a);
