b835b33ab91c9971f1a20ddf225f70338d973053
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2023-11-28 17:27:30 +0500

    dom css style

---
+++ 1) JAVASCRIPT/5-40) DOM css style/app.js
@@ -0,0 +1,35 @@
+var a;
+
+// // // .style
+// var a=document.querySelector("#header").style.border;
+// console.log(a);
+
+// document.querySelector("#header").style.backgroundColor="tan";
+// document.querySelector("#header").style.color="blue";
+// a= document.querySelector("#header").style.border
+// console.log(a);
+// a= document.querySelector("#header").style.backgroundColor
+// console.log(a);
+// a= document.querySelector("#header").style.color
+// console.log(a);
+
+// // // .class name
+a = document.querySelector("#header").className = "abc";
+document.querySelector("#header").className;
+console.log(a);
+
+// // // // .class LIST
+//  document.querySelector("#header").classList="cvb abc";
+//  a = document.querySelector("#header").classList
+// console.log(a);
+
+// // // add()is to add classes
+
+//  document.querySelector("#header").classList.add("abc","nmj")
+// a = document.querySelector("#header").classList
+// console.log(a);
+
+// document.querySelector("#header").classList.remove("lmn")
+// // uper wli line se pehle html main classes daal do
+// a = document.querySelector("#header").classList
+// console.log(a);
