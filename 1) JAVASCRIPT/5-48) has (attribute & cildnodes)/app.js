c2bf61a28f829eab397e08235d4b918246d39cc7
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2024-01-23 19:32:37 +0500

    Add files via upload

---
+++ 1) JAVASCRIPT/5-48) has (attribute & cildnodes)/app.js
@@ -0,0 +1,7 @@
+/* has Attribute*/
+      //  var b=document.getElementById("abc").hasAttribute("style");
+      //  console.log(b);
+
+      /* has child nodes*/
+      var a = document.getElementById("abc").hasChildNodes();
+      console.log(a);
