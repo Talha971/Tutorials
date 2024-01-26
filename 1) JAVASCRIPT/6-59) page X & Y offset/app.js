6024e0da9d12d777cd61507c252fefb5400a8cc5
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2024-01-27 00:02:36 +0500

    page

---
+++ 1) JAVASCRIPT/6-59) page X & Y offset/app.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", () => {
+  console.clear();
+
+  // pageYOffset
+  console.log("pageYOffset: " + window.pageYOffset);
+
+  // pageXOffset
+  console.log("pageXOffset: " + window.pageXOffset);
+});
