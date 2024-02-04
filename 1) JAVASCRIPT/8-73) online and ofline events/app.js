36dc0e4ebd498a94d361dced058dbd135301ec98
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2024-02-04 17:49:06 +0500

    online and offline events

---
+++ 1) JAVASCRIPT/8-73) online and ofline events/app.js
@@ -0,0 +1,14 @@
+// var a = document.getElementById("abc");
+// window.addEventListener("offline", () => {
+//   a.innerHTML = "ofline";
+//   a.style.backgroundColor = "red";
+// });
+// window.addEventListener("online", () => {
+//   a.innerHTML = "online";
+//   a.style.backgroundColor = "green";
+// });
+
+
+
+// agar online hoga tou true warna false show karega
+console.log(navigator.onLine);
